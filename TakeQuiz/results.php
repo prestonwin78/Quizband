2b51a1db4a6514aa36a65165b9bdd8df4a794dae
prestonwin78 <prestonwin78@gmail.com>
2020-09-03 14:37:14 -0500

    Add sample results page

---
+++ TakeQuiz/results.php
@@ -0,0 +1,10 @@
+<?php 
+    //Get quiz info from database
+    //Compare results with form data
+    //Calculate score
+    //Store user's results in database
+    echo "Hello World";
+?>
+
+
+<!-- Output score and other relevant data -->
