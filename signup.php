b72bec80231a8a8f4dce89f9fc2451e6f5a41430
prestonwin78 <prestonwin78@gmail.com>
2020-10-03 12:14:24 -0500

    Formatting

---
+++ signup.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Quizband</title>
+        
+        <!-- Bootstrap -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link rel="stylesheet" type="text/css" href="home-styles.css">
+        <script src="./home.js" defer></script>
+    </head>
+    <body>
+        <div id="main-content" class="container-fluid">
+            <div id="header" class="row bg-light">
+              <a id="title" class="text-dark" href="home.php">Quizband</a>
+                <a>Contact</a>
+                <a>About</a>
+                <a>Sign In</a>
+                <a>Sign Up</a>
+            </div>
+        </div>
+    </body>
+</html>
